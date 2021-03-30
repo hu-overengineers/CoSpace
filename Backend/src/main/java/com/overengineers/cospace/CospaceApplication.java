2893d5b3028408127286c74a2476bc3f9711c3ab
Yusuf Keten <ketenyusuf@gmail.com>
2021-03-30 17:30:40 +0300

    Spring Framework for backend is initialized.

---
+++ Backend/src/main/java/com/overengineers/cospace/CospaceApplication.java
@@ -0,0 +1,13 @@
+package com.overengineers.cospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CospaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CospaceApplication.class, args);
+	}
+
+}
